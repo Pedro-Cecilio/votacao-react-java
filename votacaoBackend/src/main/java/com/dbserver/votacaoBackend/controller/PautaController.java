4fa73fc3c1284ff44ee235da92b0bcf5a931dc0d
Pedro-Cecilio <cardev604@gmail.com>
2024-04-16 20:44:28 -0300

    :sparkles: feat: Construindo métodos da controller para listar pautas

--- votacaoBackend/src/main/java/com/dbserver/votacaoBackend/controller/PautaController.java
+++ votacaoBackend/src/main/java/com/dbserver/votacaoBackend/controller/PautaController.java
@@ -8,11 +8,16 @@ import com.dbserver.votacaoBackend.domain.pauta.dto.CriarPautaDto;
 import com.dbserver.votacaoBackend.domain.pauta.dto.RespostaPautaDto;
 import com.dbserver.votacaoBackend.domain.pauta.service.IPautaService;
 import com.dbserver.votacaoBackend.domain.usuario.Usuario;
+import com.dbserver.votacaoBackend.domain.usuario.dto.UsuarioRespostaDto;
 import com.dbserver.votacaoBackend.domain.usuario.service.IUsuarioService;
+import java.util.List;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping(value = "/pauta")
@@ -26,13 +31,34 @@ public class PautaController {
     }
 
     @PostMapping
-    public ResponseEntity<RespostaPautaDto> postMethodName(@RequestBody CriarPautaDto dto) {
+    public ResponseEntity<RespostaPautaDto> criarPauta(@RequestBody CriarPautaDto dto) {
         Usuario usuario = usuarioService.buscarUsuarioLogado();
         Pauta pauta = new Pauta(dto.assunto(), dto.categoria(), usuario);
         this.pautaService.criarPauta(pauta);
+        UsuarioRespostaDto usuarioRespostaDto = new UsuarioRespostaDto(usuario);
         RespostaPautaDto resposta = new RespostaPautaDto(pauta.getId(), pauta.getAssunto(), pauta.getCategoria(),
-                pauta.getUsuario().getId());
+                usuarioRespostaDto, null);
         return ResponseEntity.status(HttpStatus.CREATED).body(resposta);
+    }
+
+    @GetMapping("/{usuarioId}")
+    public ResponseEntity<List<RespostaPautaDto>> listarTodasPautasDoUsuario(Pageable pageable,
+            @PathVariable("usuarioId") String usuarioId) {
+        try {
+            Long usuarioIdLong = Long.parseLong(usuarioId);
+            List<Pauta> pautas = this.pautaService.buscarPautasPorUsuarioId(usuarioIdLong, pageable);
+            List<RespostaPautaDto> resposta = pautas.stream().map(RespostaPautaDto::new).toList();
+            return ResponseEntity.ok().body(resposta);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Usuário não encontrado.");
+        }
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<RespostaPautaDto>> listarTodasPautas(Pageable pageable) {
+        List<Pauta> pautas = this.pautaService.buscarTodasPautas(pageable);
+        List<RespostaPautaDto> resposta = pautas.stream().map(RespostaPautaDto::new).toList();
+        return ResponseEntity.ok().body(resposta);
     }
 
 }
--- votacaoBackend/src/main/java/com/dbserver/votacaoBackend/domain/pauta/dto/RespostaPautaDto.java
+++ votacaoBackend/src/main/java/com/dbserver/votacaoBackend/domain/pauta/dto/RespostaPautaDto.java
@@ -1,13 +1,18 @@
 package com.dbserver.votacaoBackend.domain.pauta.dto;
 
+import com.dbserver.votacaoBackend.domain.pauta.Pauta;
 import com.dbserver.votacaoBackend.domain.pauta.enums.Categoria;
+import com.dbserver.votacaoBackend.domain.sessaoVotacao.SessaoVotacao;
+import com.dbserver.votacaoBackend.domain.usuario.dto.UsuarioRespostaDto;
 
 public record RespostaPautaDto(
     Long id, 
     String assunto,
     Categoria categoria,
-    Long usuario_id
-
+    UsuarioRespostaDto usuario,
+    SessaoVotacao sessaoVotacao
 ) {
-    
+    public RespostaPautaDto(Pauta pauta){
+        this(pauta.getId(), pauta.getAssunto(), pauta.getCategoria(), new UsuarioRespostaDto(pauta.getUsuario()), pauta.getSessaoVotacao());
+    }
 }
