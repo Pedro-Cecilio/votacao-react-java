0f7d75038e8f8b5e42158bb75230bb45c336488a
Pedro-Cecilio <cardev604@gmail.com>
2024-04-19 16:33:02 -0300

    :sparkles: feat: Inserindo método de serviço que verifica se usuário pode votar e método para inserir voto do usuário

--- votacaoBackend/src/main/java/com/dbserver/votacaoBackend/domain/sessaoVotacao/service/ISessaoVotacaoService.java
+++ votacaoBackend/src/main/java/com/dbserver/votacaoBackend/domain/sessaoVotacao/service/ISessaoVotacaoService.java
@@ -1,8 +1,12 @@
 package com.dbserver.votacaoBackend.domain.sessaoVotacao.service;
 
 import com.dbserver.votacaoBackend.domain.sessaoVotacao.SessaoVotacao;
+import com.dbserver.votacaoBackend.domain.sessaoVotacao.enums.TipoDeVotoEnum;
+import com.dbserver.votacaoBackend.domain.usuario.Usuario;
 
 public interface ISessaoVotacaoService {
     public SessaoVotacao abrirVotacao(SessaoVotacao sessaoVotacao);
     public boolean verificarSeSessaoVotacaoEstaAtiva(SessaoVotacao sessaoVotacao);
+    public void verificarSeUsuarioPodeVotarSessaoVotacao(SessaoVotacao sessaoVotacao, Usuario usuario);
+    public SessaoVotacao inserirVoto(SessaoVotacao sessaoVotacao, TipoDeVotoEnum tipoDeVoto, Usuario usuario);
 }
--- votacaoBackend/src/main/java/com/dbserver/votacaoBackend/utils/Utils.java
+++ votacaoBackend/src/main/java/com/dbserver/votacaoBackend/utils/Utils.java
@@ -1,6 +1,5 @@
 package com.dbserver.votacaoBackend.utils;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.regex.Pattern;
 
