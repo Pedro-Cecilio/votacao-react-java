520986325f9e4ddc6197075dc9e63b881c8adaf1
Pedro-Cecilio <cardev604@gmail.com>
2024-04-18 22:16:17 -0300

    :sparkles: feat: Método de serviço para abrir votação

---
+++ votacaoBackend/src/main/java/com/dbserver/votacaoBackend/domain/sessaoVotacao/service/SessaoVotacaoService.java
@@ -0,0 +1,22 @@
+package com.dbserver.votacaoBackend.domain.sessaoVotacao.service;
+
+import org.springframework.stereotype.Service;
+import com.dbserver.votacaoBackend.domain.sessaoVotacao.SessaoVotacao;
+import com.dbserver.votacaoBackend.domain.sessaoVotacao.repository.SessaoVotacaoRepository;
+
+@Service
+public class SessaoVotacaoService implements ISessaoVotacaoService{
+    private SessaoVotacaoRepository sessaoVotacaoRepository;
+
+    public SessaoVotacaoService(SessaoVotacaoRepository sessaoVotacaoRepository) {
+        this.sessaoVotacaoRepository = sessaoVotacaoRepository;
+    }
+
+    @Override
+    public SessaoVotacao abrirVotacao(SessaoVotacao sessaoVotacao){
+        if(sessaoVotacao == null) throw new IllegalArgumentException("SessaoVotacao não deve ser nula.");
+        if(sessaoVotacao.getPauta().getSessaoVotacao() != null) throw new IllegalStateException("Pauta já possui uma votação aberta.");
+        
+        return this.sessaoVotacaoRepository.save(sessaoVotacao);
+    }
+}
