ec7441030cd639ae7fe400e9abb9f092af434e15
Pedro-Cecilio <cardev604@gmail.com>
2024-04-20 23:53:35 -0300

    :sparkles: feat: Construindo método de serviço para verificar status da sessão de votação

--- votacaoBackend/src/main/java/com/dbserver/votacaoBackend/domain/sessaoVotacao/service/ISessaoVotacaoService.java
+++ votacaoBackend/src/main/java/com/dbserver/votacaoBackend/domain/sessaoVotacao/service/ISessaoVotacaoService.java
@@ -1,6 +1,7 @@
 package com.dbserver.votacaoBackend.domain.sessaoVotacao.service;
 
 import com.dbserver.votacaoBackend.domain.sessaoVotacao.SessaoVotacao;
+import com.dbserver.votacaoBackend.domain.sessaoVotacao.enums.StatusSessaoVotacao;
 import com.dbserver.votacaoBackend.domain.sessaoVotacao.enums.TipoDeVotoEnum;
 import com.dbserver.votacaoBackend.domain.usuario.Usuario;
 
@@ -9,4 +10,5 @@ public interface ISessaoVotacaoService {
     public boolean verificarSeSessaoVotacaoEstaAtiva(SessaoVotacao sessaoVotacao);
     public void verificarSeUsuarioPodeVotarSessaoVotacao(SessaoVotacao sessaoVotacao, Usuario usuario);
     public SessaoVotacao inserirVoto(SessaoVotacao sessaoVotacao, TipoDeVotoEnum tipoDeVoto, Usuario usuario);
+    public StatusSessaoVotacao obterStatusSessaoVotacao(SessaoVotacao sessaoVotacao);
 }
