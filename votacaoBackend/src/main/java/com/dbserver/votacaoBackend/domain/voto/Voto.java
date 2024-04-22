c3258c244d15140e055057401d38b67d9087c005
Pedro-Cecilio <cardev604@gmail.com>
2024-04-22 18:54:02 -0300

    :sparkles: feat: Criando entidade voto

---
+++ votacaoBackend/src/main/java/com/dbserver/votacaoBackend/domain/voto/Voto.java
@@ -0,0 +1,72 @@
+package com.dbserver.votacaoBackend.domain.voto;
+
+import com.dbserver.votacaoBackend.domain.usuario.Usuario;
+import com.dbserver.votacaoBackend.utils.Utils;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Voto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String cpf;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id", nullable = true)
+    private Usuario usuario;
+
+    public Voto(String cpf, Usuario usuario) {
+        setCpf(cpf);
+        this.usuario = usuario;
+    }
+
+    public void setCpf(String cpf){
+        if(cpf == null || !Utils.validarRegex(Utils.REGEX_CPF, cpf.trim())){
+            throw new IllegalArgumentException("Cpf deve conter 11 caracteres numéricos.");
+        }
+        this.cpf = cpf.trim();
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Voto other = (Voto) obj;
+        if (cpf == null) {
+            if (other.cpf != null)
+                return false;
+        } else if (!cpf.equals(other.cpf))
+            return false;
+        return true;
+    }
+
+    
+    
+}
