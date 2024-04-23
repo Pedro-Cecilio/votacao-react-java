29e920ea4d373c18160571c16a472db2029eac67
Pedro-Cecilio <cardev604@gmail.com>
2024-04-23 14:55:08 -0300

    :recycle: ajustando migration V1 e criando migration para inserir administrador

--- votacaoBackend/src/main/resources/db/migration/V1__criando-tabelas.sql
+++ votacaoBackend/src/main/resources/db/migration/V1__criando-tabelas.sql
@@ -1,10 +1,11 @@
+-- Active: 1709668849959@@127.0.0.1@5432@testandosql@public
 CREATE TABLE usuario(
     id BIGSERIAL PRIMARY KEY,
     nome VARCHAR(20) NOT NULL,
     sobrenome VARCHAR(20) NOT NULL,
     cpf VARCHAR(11) UNIQUE NOT NULL,
     admin BOOLEAN NOT NULL
-)
+);
 
 CREATE TABLE autenticacao(
     id BIGSERIAL PRIMARY KEY,
@@ -12,36 +13,36 @@ CREATE TABLE autenticacao(
     senha VARCHAR(255) NOT NULL,
     usuario_id BIGINT UNIQUE NOT NULL,
     FOREIGN KEY (usuario_id) REFERENCES usuario(id) ON DELETE CASCADE
-)
+);
 
 CREATE TABLE voto(
     id BIGSERIAL PRIMARY KEY,
     cpf VARCHAR(11) NOT NULL,
     usuario_id BIGINT ,
     FOREIGN KEY (usuario_id) REFERENCES usuario(id) ON DELETE CASCADE
-)
+);
 
 
-CREATE TABLE sessaoVotacao(
+CREATE TABLE sessao_votacao(
     id BIGSERIAL PRIMARY KEY,
     data_abertura TIMESTAMP WITH TIME ZONE,
     data_fechamento TIMESTAMP WITH TIME ZONE,
-)
+    pauta_id BIGINT
+);
 
 CREATE TABLE sessao_votacao_votos_positivos(
     sessao_votacao_id BIGINT,
     votos_positivos_id BIGINT,
-
-    FOREIGN KEY (sessao_votacao_id) REFERENCES sessaoVotacao(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (sessao_votacao_id) REFERENCES sessao_votacao(id) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (votos_positivos_id) REFERENCES voto(id) ON DELETE CASCADE ON UPDATE CASCADE
-)
+);
 CREATE TABLE sessao_votacao_votos_negativos(
     sessao_votacao_id BIGINT,
     votos_negativos_id BIGINT,
 
-    FOREIGN KEY (sessao_votacao_id) REFERENCES sessaoVotacao(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (sessao_votacao_id) REFERENCES sessao_votacao(id) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (votos_negativos_id) REFERENCES voto(id) ON DELETE CASCADE ON UPDATE CASCADE
-)
+);
 
 CREATE TABLE pauta(
     id BIGSERIAL PRIMARY KEY,
@@ -49,8 +50,8 @@ CREATE TABLE pauta(
     categoria VARCHAR(50) NOT NULL,
     usuario_id BIGINT NOT NULL,
     FOREIGN KEY (usuario_id) REFERENCES usuario(id)
-)
+);
 
 ALTER TABLE sessao_votacao
 ADD CONSTRAINT fk_sessao_votacao_pauta
-FOREIGN KEY (pauta_id) REFERENCES sessao_votacao(id) ON UPDATE CASCADE
+FOREIGN KEY (pauta_id) REFERENCES pauta(id) ON UPDATE CASCADE;
