b99fadefbadfb0baa415983e7d985acd7f7379d8
Pedro-Cecilio <cardev604@gmail.com>
2024-04-23 16:36:21 -0300

    :recycle: refactor: removendo pasta votaçãoBackend da raiz do diretório

--- src/main/java/com/dbserver/votacaoBackend/domain/pauta/repository/PautaRepository.java
+++ src/main/java/com/dbserver/votacaoBackend/domain/pauta/repository/PautaRepository.java
@@ -3,6 +3,7 @@ package com.dbserver.votacaoBackend.domain.pauta.repository;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -18,14 +19,14 @@ public interface PautaRepository extends JpaRepository<Pauta, Long> {
     List<Pauta> findAllByUsuarioIdAndCategoria(Long usuarioId, Categoria categoria, Pageable pageable);
     Optional<Pauta> findByIdAndUsuarioId(Long id, Long usuarioId);
     @Query("SELECT p FROM Pauta p WHERE p.categoria = :categoria AND p.sessaoVotacao.dataFechamento > :dataAtual")
-    List<Pauta> findAllByCategoriaAndSessaoVotacaoAtiva(Categoria categoria, LocalDateTime dataAtual);
+    List<Pauta> findAllByCategoriaAndSessaoVotacaoAtiva(@Param("categoria") Categoria categoria, @Param("dataAtual") LocalDateTime dataAtual);
 
     @Query("SELECT p FROM Pauta p WHERE p.sessaoVotacao.dataFechamento > :dataAtual")
-    List<Pauta> findAllBySessaoVotacaoAtiva(LocalDateTime dataAtual);
+    List<Pauta> findAllBySessaoVotacaoAtiva(@Param("dataAtual") LocalDateTime dataAtual);
 
     @Query("SELECT p FROM Pauta p WHERE p.id = :id AND p.sessaoVotacao.dataFechamento > :dataAtual")
-    Optional<Pauta> findByIdAndSessaoVotacaoAtiva(Long id, LocalDateTime dataAtual);
+    Optional<Pauta> findByIdAndSessaoVotacaoAtiva(@Param("id") Long id, @Param("dataAtual") LocalDateTime dataAtual);
 
     @Query("SELECT p FROM Pauta p WHERE p.id = :id AND p.usuario.id = :usuarioId AND p.sessaoVotacao IS NOT NULL")
-    Optional<Pauta> findByIdAndUsuarioIdAndSessaoVotacaoNotNull(Long id, Long usuarioId);
+    Optional<Pauta> findByIdAndUsuarioIdAndSessaoVotacaoNotNull(@Param("id") Long id, @Param("usuarioId") Long usuarioId);
 }
